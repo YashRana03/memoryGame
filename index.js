fc986d856572807a180e2fb5c96ab73c6d4c1f60
Yash rana <97224327+YashRana03@users.noreply.github.com>
2023-09-09 10:47:45 +0100

    Refactor code, Add comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let imgArr = [
     {
         name: "bird",
-        img: "./images/bird2.png"
+        img: "./images/bird.png"
     },
 
     {
@@ -21,7 +21,7 @@ let imgArr = [
 
     {
         name: "cheese",
-        img: "./images/cheese2.png"
+        img: "./images/cheese.png"
     },
 
     {
@@ -31,7 +31,7 @@ let imgArr = [
 
     {
         name: "bird",
-        img: "./images/bird2.png"
+        img: "./images/bird.png"
     },
 
     {
@@ -51,7 +51,7 @@ let imgArr = [
 
     {
         name: "cheese",
-        img: "./images/cheese2.png"
+        img: "./images/cheese.png"
     },
 
     {
@@ -61,7 +61,7 @@ let imgArr = [
 
 ]
 
-
+// Variable initialisation
 let cardsPicked = []
 let cardsPickedId = []
 let cardMatches = 0
@@ -70,22 +70,21 @@ const blank = "./images/back.gif"
 const girdEl = document.getElementById("grid")
 const movesEl = document.getElementById("moves")
 let allCardEl;
-let count = 0
 
 
-
-
+// This creates the inital grid of cards 
 function createGrid() {
-    moves = 0
+    
     girdEl.innerHTML = ""
     console.log(girdEl)
-    count = 0
+    let count = 0
     allCardEl = []
     movesEl.textContent = 0
     cardMatches = 0
     cardsPicked = []
     cardsPickedId = []
     imgArr.sort((a, b) => 0.5 - Math.random())
+    //Creating an img element for each of the 12 cards and attaching an event listener that calls the flipCard function on click
     imgArr.forEach(()=> {
         let img = document.createElement("img")
         img.setAttribute("src", blank)
@@ -97,61 +96,39 @@ function createGrid() {
     })
 }
 
-
+// This checks whether the two cards chosen by the player are a match
 function checkMatch() {
     allCardEl = document.querySelectorAll("#grid img")
     
-    //console.log(allCardEl[cardsPickedId[0]].className)
+    //If match is found they two cards disappear
     if(cardsPicked[0] == cardsPicked[1] & cardsPickedId[0] != cardsPickedId[1]) {
         
         allCardEl[cardsPickedId[0]].style.opacity = 0
         allCardEl[cardsPickedId[1]].style.opacity = 0
-        
+
+        // cards that have been matched are marked by changing the class to "n"
         allCardEl[cardsPickedId[0]].setAttribute("class", "n")
         allCardEl[cardsPickedId[1]].setAttribute("class", "n")
         
         cardMatches++
-
-
     }
+
+    //otherwise the two cards are flipped back
     else {
         
         allCardEl[cardsPickedId[0]].setAttribute("src", blank)
         allCardEl[cardsPickedId[1]].setAttribute("src", blank)
     }
 
-    
-
+    // if there have been 6 matches, meaning the are no more cards the game ends by creating a dialogue 
     if(cardMatches == 6) {
-        const dialogueWindowEl = document.getElementById("dialogue")
-        dialogueWindowEl.setAttribute("class", "dialogue")
-        const dialogueH2El  = document.createElement("h2")
-        const dialogueH3El  = document.createElement("h3")
-        const dialogueButtonEl  = document.createElement("button")
-        const cross = document.createElement("img")
-        dialogueButtonEl.addEventListener("click", function() {
-            dialogueWindowEl.setAttribute("class", "nothing")
-            dialogueWindowEl.innerHTML = ""
-            createGrid()
-            
-        })
-        cross.src = "./images/cross.png"
-        cross.addEventListener("click", function() {
-            dialogueWindowEl.setAttribute("class", "nothing")
-            dialogueWindowEl.innerHTML = ""
-        })
-        dialogueWindowEl.appendChild(cross)
-        dialogueH2El.textContent = "Gongratulations! You Won!!!"
-        dialogueWindowEl.appendChild(dialogueH2El)
-        dialogueH3El.textContent = "Start new game?"
-        dialogueWindowEl.appendChild(dialogueH3El)
-        dialogueButtonEl.textContent = "Start Game"
-        dialogueWindowEl.appendChild(dialogueButtonEl)
-        
+        createDialogue()
     }
-
+    
+    // The two variables are reinitialised 
     cardsPicked = []
     cardsPickedId = []
+    // Each card that has not been matched has its event listener readded 
     for(let card of allCardEl) {
         if(card.getAttribute("class") == "y") {
             card.addEventListener("click", flipCard)
@@ -163,7 +140,8 @@ function checkMatch() {
 
 }
 
-
+// Displays the value of a card and if there are 2 cards flipped, calls the checkMatch function a
+// after a delay of 1.5 seconds.
 function flipCard() {
     let cardId = this.id
     
@@ -175,6 +153,8 @@ function flipCard() {
     if(cardsPicked.length == 2 ){
         moves++
         movesEl.textContent = moves
+        //Removes any event listeners from each card so that no more cards can be clicked while 
+        // timeout is on
         for(let card of allCardEl) {
             card.removeEventListener("click", flipCard)
         }
@@ -182,5 +162,39 @@ function flipCard() {
         
     }
 }
+
+// This creates the dialogue box that asks the user if they'd like to restart the game
+function createDialogue() {
+    const dialogueWindowEl = document.getElementById("dialogue")
+    dialogueWindowEl.setAttribute("class", "dialogue")
+    const dialogueH2El  = document.createElement("h2")
+    const dialogueH3El  = document.createElement("h3")
+    const dialogueButtonEl  = document.createElement("button")
+    const cross = document.createElement("img")
+    dialogueButtonEl.addEventListener("click", function() {
+        dialogueWindowEl.setAttribute("class", "nothing")
+        dialogueWindowEl.innerHTML = ""
+        createGrid()
+        
+    })
+    cross.src = "./images/cross.png"
+    cross.addEventListener("click", function() {
+        dialogueWindowEl.setAttribute("class", "nothing")
+        dialogueWindowEl.innerHTML = ""
+    })
+    dialogueWindowEl.appendChild(cross)
+    dialogueH2El.textContent = "Gongratulations! You Won!!!"
+    dialogueWindowEl.appendChild(dialogueH2El)
+    dialogueH3El.textContent = "Start new game?"
+    dialogueWindowEl.appendChild(dialogueH3El)
+    dialogueButtonEl.textContent = "Start Game"
+    dialogueWindowEl.appendChild(dialogueButtonEl)
+    
+}
+    
+
+
+
+
 
 createGrid()
